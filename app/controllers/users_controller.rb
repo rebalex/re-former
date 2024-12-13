a35df51cf02577279443d90b5c7c653324d27f4e
GreatBabySeal <143907352+GreatBabySeal@users.noreply.github.com>
2024-12-13 11:52:53 +0000

    Change from html to form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,32 @@
 class UsersController < ApplicationController
-def new
-end
+  def new
+  end
 
-def create
-end
+  def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      @user.errors.map { |e| puts e.full_message }
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  # def create
+  # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+  # if @user.save
+  # else
+  # redirect_to new_user_path
+  #   render :new, status: :unprocessable_entity
+  # end
+  # end
 end
